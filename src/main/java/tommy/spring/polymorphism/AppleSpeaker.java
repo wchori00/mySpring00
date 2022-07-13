4443e6967391a5fd4f6495831ea76eddc5ed951f
wchori00 <swc198888@gmail.com>
2022-07-13 16:49:26 +0900

    Spring 어노테이션 기반 설정, 의존성 주입 설정, 어노테이션과 XML 설정 병행하여 사용하기 예제 실습

--- src/main/java/tommy/spring/polymorphism/AppleSpeaker.java
+++ src/main/java/tommy/spring/polymorphism/AppleSpeaker.java
@@ -1,5 +1,8 @@
 package tommy.spring.polymorphism;
 
+import org.springframework.stereotype.Component;
+
+//@Component("apple")
 public class AppleSpeaker implements Speaker {
 	public AppleSpeaker() {
 		System.out.println("AppleSpeaker °´Ã¼ »ý¼º");
--- src/main/java/tommy/spring/polymorphism/LgTV.java
+++ src/main/java/tommy/spring/polymorphism/LgTV.java
@@ -1,6 +1,14 @@
 package tommy.spring.polymorphism;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component("tv")
 public class LgTV implements TV {
+	@Autowired
+//	@Qualifier("apple")
+//	@Resource(name="apple")
+	private Speaker speaker;
 	
 	public LgTV() {
 		System.out.println("LG TV 객체 생성");
@@ -12,9 +20,11 @@ public class LgTV implements TV {
 		System.out.println("LgTV 전원을 끈다");
 	}
 	public void volumeUp() {
-		System.out.println("LgTV 볼륨을 올린다");
+//		System.out.println("LgTV 볼륨을 올린다");
+		speaker.volumeUp();
 	}
 	public void volumeDown() {
-		System.out.println("LgTV 볼륨을 내린다");
+//		System.out.println("LgTV 볼륨을 내린다");
+		speaker.volumeDown();
 	}
 }
