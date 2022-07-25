2c96b5b87f214f81d65091ff650f9021e12648ce
wchori00 <swc198888@gmail.com>
2022-07-25 14:45:10 +0900

    Spring MVC 어노테이션 기반 설정 예제 실습

--- src/main/java/tommy/spring/web/board/BoardController.java
+++ src/main/java/tommy/spring/web/board/BoardController.java
@@ -1,14 +1,26 @@
 package tommy.spring.web.board;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import tommy.spring.web.board.impl.BoardDAO;
 
 @Controller
+@SessionAttributes("board")
 public class BoardController {
-	
+	@ModelAttribute("conditionMap")
+	public Map<String, String> searchConditionMap() {
+		Map<String, String> conditionMap = new HashMap<String, String>();
+		conditionMap.put("제목", "TITLE");
+		conditionMap.put("내용", "CONTENT");
+		return conditionMap;
+	}
 	@RequestMapping("/insertBoard.do")
 	public String insertBoard(BoardVO vo, BoardDAO boardDAO) {
 		System.out.println("글 등록 처리");
@@ -17,8 +29,15 @@ public class BoardController {
 	}
 	
 	@RequestMapping("/updateBoard.do")
-	public String updateBoard(BoardVO vo, BoardDAO boardDAO) {
+	public String updateBoard(@ModelAttribute("board") BoardVO vo, BoardDAO boardDAO) {
 		System.out.println("글 수정 기능 처리");
+		System.out.println("번호: " + vo.getSeq());
+		System.out.println("제목: " + vo.getTitle());
+		System.out.println("작성자: " + vo.getWriter());
+		System.out.println("내용: " + vo.getContent());
+		System.out.println("등록일: " + vo.getRegDate());
+		System.out.println("조회수: " + vo.getCnt());
+		System.out.println("작성자 이름: " + vo.getWriter());
 		boardDAO.updateBoard(vo);
 		return "getBoardList.do";
 	}
@@ -31,19 +50,21 @@ public class BoardController {
 	}
 	
 	@RequestMapping("/getBoard.do")
-	public ModelAndView getBoard(BoardVO vo, BoardDAO boardDAO, ModelAndView mav) {
+	public String getBoard(BoardVO vo, BoardDAO boardDAO, Model model) {
 		System.out.println("글 상세 보기 처리");
-		mav.addObject("board", boardDAO.getBoard(vo));
-		mav.setViewName("getBoard.jsp");
-		return mav;
+		model.addAttribute("board", boardDAO.getBoard(vo));
+		return "getBoard.jsp";
 	}
 	
 	@RequestMapping("/getBoardList.do")
-	public ModelAndView getBoardList(BoardVO vo, BoardDAO boardDAO, ModelAndView mav) {
+	public String getBoardList(BoardVO vo, BoardDAO boardDAO, Model model) {
 		System.out.println("글 목록 검색 처리");
-		mav.addObject("boardList", boardDAO.getBoardList(vo));	// Model 정보저장
-		mav.setViewName("getBoardList.jsp");		// View 정보저장
-		return mav;
+		System.out.println("검색 조건: " + vo.getSearchCondition());
+		System.out.println("검색 단어: " + vo.getSearchKeyword());
+//		mav.addObject("boardList", boardDAO.getBoardList(vo));	// Model 정보저장
+//		mav.setViewName("getBoardList.jsp");		// View 정보저장
+		model.addAttribute("boardList", boardDAO.getBoardList(vo));
+		return "getBoardList.jsp";
 	}
 	
 }
--- src/main/java/tommy/spring/web/user/LoginController.java
+++ src/main/java/tommy/spring/web/user/LoginController.java
@@ -1,21 +1,28 @@
 package tommy.spring.web.user;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import tommy.spring.web.user.impl.UserDAO;
 
 @Controller
 public class LoginController {
-	@RequestMapping("/login.do")
+	@RequestMapping(value="/login.do", method=RequestMethod.GET)
+	public String loginView(UserVO vo) {
+		System.out.println("로그인 화면으로 이동");
+		vo.setId("test");
+		vo.setPassword("test");
+		return "login.jsp";
+	}
+	
+	@RequestMapping(value="/login.do", method=RequestMethod.POST)
 //	public String handleRequest(HttpServletRequest request, HttpServletResponse response) {
 //	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) {
-	public String login(UserVO vo, UserDAO userDAO) {
-		System.out.println("로그인 처리");
+	public String login(UserVO vo, UserDAO userDAO, HttpSession session) {
+		System.out.println("로그인 인증 처리");
 		// 1. 사용자 입력 정보 추출
 //		String id = request.getParameter("id");
 //		String password = request.getParameter("password");		
@@ -26,12 +33,13 @@ public class LoginController {
 //		vo.setPassword(password);
 		
 //		UserDAO userDAO = new UserDAO();
-//		UserVO user = userDAO.getUser(vo);
+		UserVO user = userDAO.getUser(vo);
 		
 		// 3. 화면 네비게이션
 //		ModelAndView mav = new ModelAndView();
-//		if(user != null) {
-		if(userDAO.getUser(vo) != null) {
+		if(user != null) {
+//		if(userDAO.getUser(vo) != null) {
+			session.setAttribute("userName", user.getName());
 			return "getBoardList.do";
 //			//mav.setViewName("getBoardList.do");
 //			mav.setViewName("redirect:getBoardList.do");
